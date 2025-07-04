063de8bf588007f57fe2715498646f7b99d6b628
Avela <avelankanini745@gmail.com>
2025-07-04 03:03:23 +0200

    updated fruits java
    Co-Authored-By: Marvista200 <219212600+Marvista200@users.noreply.github.com>

--- fruits.java
+++ fruits.java
@@ -2,5 +2,7 @@ public class MyClass {
     public static void main(String[] args) {
         System.out.println("This is an apple.");
         System.out.println("This is a banana.");
+
+         System.out.println("This is an orange.");
     }
 }
